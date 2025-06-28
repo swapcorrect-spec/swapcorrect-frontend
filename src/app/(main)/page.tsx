79acdeaa1bc78896574b59e697809a723f7ed102
Nelson-Chinedu <egwuchinedu69@gmail.com>
2025-06-28 17:43:28 +0100

    feat: item listing

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,8 +1,3 @@
-import { useRouter } from "next/navigation";
-import { cn } from "@/lib/utils";
-import Sidebar from "@/components/shared/sidebar";
-import Navbar from "@/components/shared/navbar";
-import { PATHS } from "../_constants/paths";
 import Main from "@/app/(main)/main";
 
 export default function MainPage({
@@ -10,10 +5,5 @@ export default function MainPage({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  // const router = useRouter();
-  // const isLoggedIn = localStorage.getItem("user") ?? null;
-  // if (!isLoggedIn) {
-  //   router.push(`/${PATHS.LOGIN}`);
-  // }
   return <Main>{children}</Main>;
 }
