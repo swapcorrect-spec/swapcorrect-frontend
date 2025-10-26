2765a4bf740ebcca04fb50d5fa422a3500a2ff0c
Nelson-Chinedu <egwuchinedu69@gmail.com>
2025-10-26 12:15:00 +0100

    feat: add page footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,53 +12,54 @@ import {
 import { Auth } from "./_config/auth";
 import { PATHS } from "./_constants/paths";
 import { redirect } from "next/navigation";
-
+import Footer from "@/components/shared/footer";
 
 export default function Home() {
   const { isLoading: isLoadingHotPicks, data } = useGetItemByRaterHotPick({
     enabler: true,
   });
-  const { isLoading: isLoadingRecommendedItems, data: recommendedItems } =
-    useGetRecommendedItems({ enabler: true });
-  const { isLoading: isLoadingElectronicsItems, data: electronicsItems } =
-    useGetElectronicsItems({ enabler: true });
+  const { isLoading: isLoadingRecommendedItems, data: recommendedItems } = useGetRecommendedItems({ enabler: true });
+  const { isLoading: isLoadingElectronicsItems, data: electronicsItems } = useGetElectronicsItems({ enabler: true });
   const isAuthenticated = Auth.isAuthenticated();
   if (isAuthenticated) {
     redirect(`${PATHS.DASHBOARD}`);
   }
   return (
-    <div>
-      <Navbar />
-      <Herosection />
-      <div className="my-8">
-        <div className="w-[90%] right-0 absolute gap-12 flex flex-col">
-          <Marketplace
-            title="FEATURED"
-            subtitle="Hot Picks, Fast Swaps."
-            description="Discover trending items that everyone wants — swap quick"
-            products={data || HOT_PICKS}
-            isLoading={isLoadingHotPicks}
-            isAuthenticated={isAuthenticated}
-          />
-          <Marketplace
-            title="FEATURED"
-            subtitle="Swaps Just for You."
-            description="Our spotlight trades are secure, high-value, and worth every click."
-            products={recommendedItems || HOT_PICKS}
-            isLoading={isLoadingRecommendedItems}
-            isAuthenticated={isAuthenticated}
-          />
-          <Marketplace
-            title="OUR RECOMMENDATION"
-            subtitle="Advanced Tech Gadgets"
-            description="Our spotlight trades are secure, high-value, and worth every click."
-            products={electronicsItems || HOT_PICKS}
-            showSliderArrows
-            isLoading={isLoadingElectronicsItems}
-            isAuthenticated={isAuthenticated}
-          />
+    <>
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <Herosection />
+        <div className="flex-1 flex justify-end">
+          <div className="my-8 w-[90%] ml-auto mr-10 gap-12">
+            <Marketplace
+              title="FEATURED"
+              subtitle="Hot Picks, Fast Swaps."
+              description="Discover trending items that everyone wants — swap quick"
+              products={data || HOT_PICKS}
+              isLoading={isLoadingHotPicks}
+              isAuthenticated={isAuthenticated}
+            />
+            <Marketplace
+              title="FEATURED"
+              subtitle="Swaps Just for You."
+              description="Our spotlight trades are secure, high-value, and worth every click."
+              products={recommendedItems || HOT_PICKS}
+              isLoading={isLoadingRecommendedItems}
+              isAuthenticated={isAuthenticated}
+            />
+            <Marketplace
+              title="OUR RECOMMENDATIONs"
+              subtitle="Advanced Tech Gadgets"
+              description="Our spotlight trades are secure, high-value, and worth every click."
+              products={electronicsItems || HOT_PICKS}
+              showSliderArrows
+              isLoading={isLoadingElectronicsItems}
+              isAuthenticated={isAuthenticated}
+            />
+          </div>
         </div>
+        <Footer />
       </div>
-    </div>
+    </>
   );
 }
